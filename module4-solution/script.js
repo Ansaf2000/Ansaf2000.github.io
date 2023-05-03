d913db7882b5b25d147e0c2e69154dc05ff10235
Ansaf2000 <127641095+Ansaf2000@users.noreply.github.com>
2023-05-03 12:20:05 +0530

    update

---
+++ module4-solution/script.js
@@ -0,0 +1,20 @@
+var names=new Array();
+names[0]="Yaakov";
+names[1]="Jen";
+names[2]="John";
+names[3]="jeson";
+names[4]="paul";
+names[5]="frank";
+names[6]="jimmy";
+names[7]="Laura";
+names[8]="frank";
+names[9]="jason";
+
+for (var i =0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+       console.log("Goodbye "+ names[i])
+	}
+     else{
+     	  console.log("hello "+ names[i])
+     }
+}
